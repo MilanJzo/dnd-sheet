195543f3bb9d3661ed7c05c134d383e76c1a78c3
MilanJzo <73588749+MilanJzo@users.noreply.github.com>
2023-07-26 17:57:00 +0200

    continued nav development

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,20 @@
 
 <div>
     <img src="/Dragon.svg" alt="Logo" width="50">
-    <h1>DnD Sheet</h1>
+    <div>
+        <h1>DnD Sheet</h1>
+
+        <div>
+            <a href="/">Home</a>
+            <a href="/characters">Characters</a>
+            <a href="/settings">Settings</a>
+        </div>
+    </div>
 </div>
 
-<style>
+<style lang="scss">
+
+
     div {
         padding: 0;
         margin: 0;
@@ -18,11 +28,35 @@
         display: flex;
         flex-direction: row;
         align-items: center;
-        gap: 10px;
-
-        /* background-color: red; */
+        gap: 20px;
 
         border-bottom: 2px solid black;
+
+        & div{
+            padding: 0;
+            margin: 0;
+
+            width: fit-content;
+            height: 100%;
+
+            align-items: last baseline;
+
+            border: none;
+        }
+    }
+
+    a {
+        margin: 0;
+        padding: 0;
+
+        text-decoration: none;
+        color: black;
+
+        font-size: 1.25rem;
+
+        &:hover {
+            color: darkblue;
+        }
     }
 </style>
 
