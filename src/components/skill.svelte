b54593be6f7c3bb1f6812e539ece6fc6323ca567
MilanJzo <milan.jezovsek@gmail.com>
2023-09-25 20:33:50 +0200

    skill madness 1

--- src/components/skill.svelte
+++ src/components/skill.svelte
@@ -56,67 +56,8 @@
         width: 100%;
         height: fit-content;
 
-        margin: 0;
         padding: 5px;
-        padding-inline: 10px;
 
-        display: flex;
-        gap: 10px;
-
-        border: 1px solid var(--text);
-        border-radius: 5px;
-
-        & p {
-            padding: 0;
-            margin: 0;
-
-            font-size: 0.8rem;
-        }
-    }
-
-    .wrap {
-        width: 100px;
-        height: 80%;
-
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        gap: 5px;
-    }
-
-    .select {
-        width: auto;
-        height: 15px;
-
-        padding: 0;
-        padding-inline: 5px;
-
-        font-size: 0.7rem;
-
-        border: none;
-
-        background-color: var(--primary);
-    }
-
-    .skillLa {
-        width: auto;
-        height: 20px;
-
-        padding: 0;
-        padding-inline: 3px;
-
-        border: 1px solid var(--text);
-        border-radius: 5px;
-
-        background-color: var(--primary);
-
-        font-size: var(--text-size);
-    }
-
-    .input {
-        width: 100%;
-        height: 21px;
-
-        padding: 0;
+        background: red;
     }
 </style>
